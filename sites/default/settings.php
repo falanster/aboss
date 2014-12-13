86353222b8e0223edbb13c9244f7f857ab706b54
ABOSS <aboss@aboss.by>
2014-12-13 12:07:59 +0300

    entity reference

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -4,6 +4,7 @@
  * @file
  * Drupal site-specific configuration file.
  *
+ *
  * IMPORTANT NOTE:
  * This file may have been set to read-only by the Drupal installation program.
  * If you make changes to this file, be sure to protect it again after making
